026ac28462f7f81e35af42d52e9b97d547025783
massimoalbarello <massimo00albarello@gmail.com>
2024-06-19 17:48:18 +0100

    chore: disable Suspense boundary

--- papi_frontend/next.config.mjs
+++ papi_frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+      missingSuspenseWithCSRBailout: false, // TODO: fix this (https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout)
+    },
+  };
 
 export default nextConfig;
