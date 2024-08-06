7857c9fa6c87c65c52f40c3fe1cccd5c972ae45a
massimoalbarello <massimo00albarello@gmail.com>
2024-08-06 16:41:56 -0700

    feat: create table if does not exist yet

--- papi_backend/src/auth_db_client/mod.rs
+++ papi_backend/src/auth_db_client/mod.rs
@@ -1,18 +1,72 @@
 use crate::api::types::OAuthInfo;
-use aws_sdk_dynamodb::{types::AttributeValue, Client};
+use aws_sdk_dynamodb::{
+    error::SdkError,
+    operation::create_table::CreateTableError,
+    types::{
+        AttributeDefinition, AttributeValue, BillingMode, KeySchemaElement, KeyType,
+        ScalarAttributeType,
+    },
+    Client,
+};
 use serde_dynamo::{from_item, to_item};
 use std::env;
 
 pub struct AuthDbClient {
     client: Client,
+    table_name: String,
+}
+
+async fn create_table(
+    client: &Client,
+    table_name: &str,
+    key: &str,
+) -> Result<(), SdkError<CreateTableError>> {
+    // partition key
+    let attr_part = AttributeDefinition::builder()
+        .attribute_name(key)
+        .attribute_type(ScalarAttributeType::S)
+        .build()?;
+
+    let ks = KeySchemaElement::builder()
+        .attribute_name(key)
+        .key_type(KeyType::Hash)
+        .build()?;
+
+    client
+        .create_table()
+        .table_name(table_name)
+        .billing_mode(BillingMode::PayPerRequest)
+        .attribute_definitions(attr_part)
+        .key_schema(ks)
+        .send()
+        .await?;
+
+    Ok(())
 }
 
 impl AuthDbClient {
     pub async fn setup() -> Result<Self, String> {
+        let table_name =
+            env::var("DYNAMO_DB_AUTH_TABLE_NAME").expect("DYNAMO_DB_AUTH_TABLE_NAME must be set");
+
         let config = aws_config::load_from_env().await;
         let client = Client::new(&config);
 
-        Ok(Self { client })
+        if !client
+            .list_tables()
+            .send()
+            .await
+            .map_err(|e| format!("Error listing tables: {}", e.to_string()))?
+            .table_names()
+            .contains(&table_name)
+        {
+            create_table(&client, &table_name, "user_id")
+                .await
+                .map_err(|e| format!("Error creating table: {}", e))?;
+            println!("Created table: {}", table_name);
+        }
+
+        Ok(Self { client, table_name })
     }
 
     pub async fn create_auth(&self, oauth_info: OAuthInfo) -> Result<(), String> {
@@ -21,10 +75,7 @@ impl AuthDbClient {
 
         self.client
             .put_item()
-            .table_name(
-                env::var("DYNAMO_DB_AUTH_TABLE_NAME")
-                    .expect("DYNAMO_DB_AUTH_TABLE_NAME must be set"),
-            )
+            .table_name(&self.table_name)
             .item("user_id", AttributeValue::S(oauth_info.user_id()))
             .set_item(Some(item))
             .send()
@@ -39,10 +90,7 @@ impl AuthDbClient {
         let query_output = self
             .client
             .query()
-            .table_name(
-                env::var("DYNAMO_DB_AUTH_TABLE_NAME")
-                    .expect("DYNAMO_DB_AUTH_TABLE_NAME must be set"),
-            )
+            .table_name(&self.table_name)
             .key_condition_expression("user_id = :user_id")
             .expression_attribute_values(":user_id", AttributeValue::S(user_id.clone()))
             .scan_index_forward(false)
@@ -71,10 +119,7 @@ impl AuthDbClient {
 
         self.client
             .put_item()
-            .table_name(
-                env::var("DYNAMO_DB_AUTH_TABLE_NAME")
-                    .expect("DYNAMO_DB_AUTH_TABLE_NAME must be set"),
-            )
+            .table_name(&self.table_name)
             .item("user_id", AttributeValue::S(user_id))
             .set_item(Some(item))
             .send()
